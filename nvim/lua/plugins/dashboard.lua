2a07b895f3a4ef2795686f4ea109ea42cd2db25c
walid-hud <walidthehud@gmail.com>
2026-05-08 15:36:11 +0100

    new git alias

---
+++ nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          keys = {},
+          header = [[The quieter you become, the more you can hear]],
+        },
+        sections = {
+          { section = "header" },
+        },
+      },
+    },
+  },
+}
